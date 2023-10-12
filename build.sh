9b4e0d00145b8271b82dba3b3278c28c0ab52ea3
Giovanni Visciano <giovanni_visciano@yahoo.it>
2023-10-12 08:47:32 +0200

    dev

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -e
 VERSION=$(grep -oE 'version: ("[0-9]+\.[0-9]+\.[0-9]+")' mix.exs | cut -d '"' -f 2)
 
 docker buildx build --file Dockerfile.bootstrap --target app --tag dake:latest .
-docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:$PWD -w $PWD dake:latest run elixir.lint
-docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:$PWD -w $PWD dake:latest run --tag "dake:$VERSION" elixir.escript
-docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:$PWD -w $PWD dake:$VERSION run --tag "dake:latest" elixir.escript
+
+DOCKER_RUN="docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock -v '$PWD:$PWD' -w '$PWD'"
+eval "$DOCKER_RUN dake:latest run elixir.lint"
+eval "$DOCKER_RUN dake:latest run --tag dake:$VERSION elixir.escript"
+eval "$DOCKER_RUN dake:$VERSION run --tag dake:latest elixir.escript"
