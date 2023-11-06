0084740eaec4c3efb3e2bc131590212645d2849d
Giovanni Visciano <giovanni_visciano@yahoo.it>
2023-11-06 21:45:38 +0100

    dev

--- dake.sh
+++ dake.sh
@@ -4,19 +4,22 @@ set -ex
 
 DAKE_IMAGE=${DAKE_IMAGE:-"dake:latest"}
 
-case $(uname) in
-    Darwin) SSH_AUTH_SOCK=/run/host-services/ssh-auth.sock ;;
-    Linux) SSH_AUTH_SOCK="$SSH_AUTH_SOCK" ;;
-esac
 
 if [ "$CI" = "true" ]; then
-    OPTS=""
+    docker run --init --rm --network=host \
+        -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w "$PWD" \
+        -e LOG_LEVEL=${LOG_LEVEL:-notice} \
+        $DAKE_IMAGE "$@"
 else
-    OPTS="-ti"
+    case $(uname) in
+        Darwin) SSH_AUTH_SOCK=/run/host-services/ssh-auth.sock ;;
+        Linux) SSH_AUTH_SOCK="$SSH_AUTH_SOCK" ;;
+    esac
+
+    docker run --init --rm -ti --network=host \
+        -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w "$PWD" \
+        -e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" -v "$SSH_AUTH_SOCK:$SSH_AUTH_SOCK" \
+        -e LOG_LEVEL=${LOG_LEVEL:-notice} \
+        $DAKE_IMAGE "$@"
 fi
 
-docker run $OPTS --init --rm --network=host \
-    -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w "$PWD" \
-    -e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" -v "$SSH_AUTH_SOCK:$SSH_AUTH_SOCK" \
-    -e LOG_LEVEL=${LOG_LEVEL:-notice} \
-    $DAKE_IMAGE "$@"
