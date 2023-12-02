78faff2c988bd06cb8d18d9b36606dc1cde96094
Giovanni Visciano <giovanni_visciano@yahoo.it>
2023-12-02 21:32:59 +0100

    dev

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -e
 
 # bootstrap
 CAKE_VERSION=${CAKE_VERSION:-latest}
-docker build --file Containerfile.bootstrap --build-arg CAKE_VERSION=${CAKE_VERSION} --target cake.app --tag cake:latest .
+docker build --file Containerfile --build-arg CAKE_VERSION=${CAKE_VERSION} --target cake.app --tag cake:latest .
 
 # cake building cake
 source priv/source.sh
-cake run --verbose elixir.lint
+cake run --verbose all
 cake run --tag cake:latest cake.app
