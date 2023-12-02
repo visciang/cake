6168c3a305dfe24be639267cedf5dffafa13b0a7
Giovanni Visciano <giovanni_visciano@yahoo.it>
2023-12-02 22:20:00 +0100

    done

--- priv/source.fish
+++ priv/source.fish
@@ -11,7 +11,13 @@ function cake
         set SSH_AUTH_SOCK "/run/host-services/ssh-auth.sock"
     end
 
-    docker run --init --rm -ti --network=host \
+    if test "$CI" = "true"
+        set TI ""
+    else
+        set TI "-ti"
+    end
+
+    docker run --init --rm $TI --network=host \
         -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w "$PWD" \
         -e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" -v "$SSH_AUTH_SOCK:$SSH_AUTH_SOCK" \
         -e LOG_LEVEL="$LOG_LEVEL" \
