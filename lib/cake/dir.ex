3581f8bc092b3f6b0dce344566dfae9d27ec81f4
Giovanni Visciano <giovanni_visciano@yahoo.it>
2024-01-19 22:11:48 +0100

    dev

--- lib/cake/dir.ex
+++ lib/cake/dir.ex
@@ -34,16 +34,40 @@ defmodule Cake.Dir do
     :ok
   end
 
-  @external_resource "lib/cake/pipeline/cake_cmd.sh"
-  @cmd_wrapper_script File.read!("lib/cake/pipeline/cake_cmd.sh")
+  @cmd_wrapper """
+  #!/usr/bin/env bash
+
+  # Ref: https://hexdocs.pm/elixir/Port.html#module-zombie-operating-system-processes
+
+  # Start the program in the background
+  exec "$@" &
+  pid1=$!
+
+  # Silence warnings from here on
+  exec >/dev/null 2>&1
+
+  # Read from stdin in the background and
+  # kill running program when stdin closes
+  exec 0<&0 $(
+    while read; do :; done
+    kill -KILL $pid1
+  ) &
+  pid2=$!
+
+  # Clean up
+  wait $pid1
+  ret=$?
+  kill -KILL $pid2
+  exit $ret
+  """
 
   @spec install_cmd_wrapper_script :: :ok
   def install_cmd_wrapper_script do
-    path = Path.join(System.tmp_dir!(), "cake_cmd.sh")
+    path = Path.join(System.tmp_dir!(), "cmd_wrapper.sh")
 
     :persistent_term.put(:cake_cmd_wrapper_path, path)
 
-    File.write!(path, @cmd_wrapper_script)
+    File.write!(path, @cmd_wrapper)
     File.chmod!(path, 0o700)
 
     :ok
