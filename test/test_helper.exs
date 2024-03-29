a734cca6b341422dc5846ae794963a2fa0663f35
Giovanni Visciano <giovanni_visciano@yahoo.it>
2024-03-29 22:01:09 +0100

    test timeout

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,4 +4,4 @@ Application.put_env(:cake, :container_manager, Test.ContainerManagerMock)
 Mox.defmock(Test.SystemBehaviourMock, for: Cake.SystemBehaviour)
 Application.put_env(:cake, :system_behaviour, Test.SystemBehaviourMock)
 
-ExUnit.start(capture_log: true)
+ExUnit.start(capture_log: true, assert_receive_timeout: 1_000, refute_receive_timeout: 1_000)
